823477dd248394ad91682c8ee4e7352e38bde4d8
Nicolas Limage <github@xephon.org>
2016-08-01 12:12:12 +0000

    improved logging

--- sysassert/plugin/__init__.py
+++ sysassert/plugin/__init__.py
@@ -5,7 +5,7 @@ from ..tools import DictListComparator, inline_dict
 class AssertPlugin(object):
 
     def __init__(self):
-        self.log = logging.getLogger()
+        self.log = logging.getLogger(__name__)
 
     def validate(self, spec, strict=True):
         raise NotImplemented
@@ -15,14 +15,23 @@ class AssertPlugin(object):
 
     @staticmethod
     def get_datasource(name):
+        log = logging.getLogger(__name__)
+        log.debug('searching datasource "{}"'.format(name))
         for entrypoint in pkr.iter_entry_points('sysassert_datasource_v1'):
             if name == entrypoint.name:
+                log.debug('found datasource "{}"'.format(name))
                 return entrypoint.load()
+        log.debug('datasource "{}" not found'.format(name))
         return None
 
     def datasource_validate(self, spec, datasource, elttype=None, strict=True):
         items = self.get_datasource(datasource)().get_items(elttype)
         dlc = DictListComparator(spec, items)
+
+        self.log.debug('validating from "{}/{}" datasource (strict: {})'
+                       .format(datasource,
+                               elttype if elttype is not None else '',
+                               strict))
 
         for item in dlc.found:
             self.log.info('found matching {} ({})'.format(elttype or 'device',
