7a921155220f76bfb92303444da0eb0f2c1b4e5d
Nicolas Limage <github@xephon.org>
2016-12-31 09:34:43 +0100

    fix path expansion in commands

--- sysassert/cmd.py
+++ sysassert/cmd.py
@@ -1,13 +1,25 @@
 import subprocess
+import os
+
+def pathexpand(command):
+    defaultpath = '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin'
+    command = list(command)
+    if not command[0].startswith('/'):
+        for path in os.environ.get('PATH', defaultpath).split(':'):
+            filename = '{0}/{1}'.format(path, command[0])
+            if os.path.exists(filename):
+                command[0] = filename
+                break
+    return command
 
 def rawcmd(command):
-    process = subprocess.Popen(command, shell=False,
+    process = subprocess.Popen(pathexpand(command), shell=False,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.STDOUT)
     return process.stdout.read().decode()
 
 def cmd(command):
-    process = subprocess.Popen(command, shell=False,
+    process = subprocess.Popen(pathexpand(command), shell=False,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.STDOUT)
     return (process.wait(), [line.decode().strip('\n')
