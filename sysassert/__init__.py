7ed2edb906bc18e3683e27d51b304945c1beaca4
Nicolas Limage <github@xephon.org>
2017-09-27 14:37:48 +0000

    bump version

--- sysassert/__init__.py
+++ sysassert/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "Nicolas Limage"
 __license__ = "GPL"
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 __maintainer__ = "Nicolas Limage"
 __email__ = "github@xephon.org"
 __status__ = "Production"
